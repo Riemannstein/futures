0174efbadb6f13de265e19aecee94c291c529d67
Riemannstein <houyifeng1005@hotmail.com>
2017-04-06 18:00:08 +0800

    update

--- ladder/analysis.py
+++ ladder/analysis.py
@@ -4,30 +4,34 @@ import numpy as np
 import itertools
 from param import *
 
-# Load the data
+# Load the raw data
 df = pd.read_csv(ticker+".csv", encoding = "GBK")
 # Delete the first two rows
 df = df.iloc[2:]
 
+# Load additional data
 position_stack = np.loadtxt("./data/position_stack.txt")
 profit_tick = np.loadtxt("./data/profit_tick.txt")
+book = np.loadtxt("./data/book.txt")
 cum_profit = sum(profit_tick) # cumulative profit from ticks
 profit_before = np.zeros(len(df))
 
+
+
+# Compute the cumulative profit series for each tick
 for i in range(len(profit_tick)):
 	if i == 0:
 		profit_before[i] = profit_tick[i]
 	else:
 		profit_before[i] = profit_before[i-1] + profit_tick[i]
 
-
-
+# Setting data length according to debugging mode
 if debug == 1:
 	data_len = 5
 else:
 	data_len = len(df)
 	
-close_profit = np.zeros(data_len)
+close_profit = np.zeros(len(position_stack))
 
 # Calculate the profit for closing positions at the end of the day
 for i in range(len(position_stack)):
@@ -46,27 +50,37 @@ profit_after[-1] = cum_profit
 print("The profit from closing positions is ", sum(close_profit))
 print("The profit from ticks is ", sum(profit_tick))
 print("The cumulative profit is ", sum(profit_tick) + sum(close_profit))
+print("The cumulative profit is ", cum_profit)
 print("Number of naked positions is ", len(position_stack))
 
 # Plot the price series
 plt.xlabel("Tick")
 plt.ylabel("Lase price")
 plt.plot(df.lastPrice)
+plt.plot(df.askPrice1)
+plt.plot(df.bidPrice1)
 plt.savefig("./plot/lastPrice.eps", format="eps")
 plt.close()
 
 # Plot the net profit series before closing positions
 plt.xlabel("Tick")
 plt.ylabel("Net proftit before closing positions")
-plt.plot(profit_before)
+plt.plot(profit_before, linewidth = 0.3)
 plt.savefig("./plot/profit_before.eps", format="eps")
 plt.close()
 
 # Plot the net profit series after closing positions
 plt.xlabel("Tick")
 plt.ylabel("Net profit after closing positions")
-plt.plot(profit_after)
+plt.plot(profit_after, linewidth = 0.3)
 plt.savefig("./plot/profit_after.eps", format="eps")
+plt.close()
+
+# Plot the net profit series before closing positions
+plt.xlabel("Tick")
+plt.ylabel("Book series")
+plt.plot(book, linewidth = 0.3)
+plt.savefig("./plot/book.eps", format="eps")
 plt.close()
 
 print(position_stack)
--- ladder/main.py
+++ ladder/main.py
@@ -29,6 +29,8 @@ long_position = np.zeros(data_len, dtype="int")
 short_price = np.zeros(data_len, dtype="int")
 long_price = np.zeros(data_len, dtype="int")
 position_stack = np.array([])
+book = np.zeros(len(df))
+stack_len = np.zeros(len(df))
 cum_profit = int(0) # cumulative profit
 profit_tick = np.zeros(len(df)) # profit series for trading in certain ticks 
 commission = np.zeros(len(df)) # commission for each trade
@@ -39,6 +41,13 @@ for i in range(data_len):
 	# For the first period, just set my bid and ask
 	print("\nData time is ", df.iloc[i]["dataTime"])
 	if i == 0:
+		# Initialize the book value of the first tick
+		book[i] = 0 
+		
+		# Intialize the stack length series
+		stack_len[i] = 0
+ 		
+		# Place limited orders
 		my_bid = df.iloc[i]["bidPrice1"] 
 		my_ask = df.iloc[i]["askPrice1"] 
 		print("My bid price at time", df.iloc[i]["dataTime"], " is ", my_bid)
@@ -108,6 +117,28 @@ for i in range(data_len):
 				# Append 
 				position_stack = np.hstack((position_stack, my_bid))
 				print("Append long position: ", my_bid)
+		
+		## Record the stack length 
+		#stack_len[i] = len(position_stack)
+				
+		# Update the book series
+		# If not holding any position
+		if len(position_stack) == 0:
+			book[i] = cum_profit
+		# If the position has changed
+		else:
+			if position_stack[-1] > 0:
+				book[i] = sum(df.iloc[i]["lastPrice"] - position_stack) + cum_profit
+			elif position_stack[-1] < 0:
+				book[i] = sum( - df.iloc[i]["lastPrice"] - position_stack) + cum_profit
+		##elif stack_len[i] != stack_len[i-1]:
+			#for i in range(len(position_stack)):
+				#if position_stack[i] > 0:
+					#book[i] = book[i] + (df.iloc[i]["lastPrice"] - position_stack[i])
+				#elif position_stack[i] < 0:
+					#book[i] = book[i] + ( abs(position_stack[i]) - df.iloc[i]["lastPrice"])
+			## Add the cumulative profit
+			#book[i] += cum_profit
 			
 	if debug == 1:
 		print("Position stack is ", position_stack)
@@ -124,6 +155,7 @@ if debug == 1:
 # Save data for analysis
 np.savetxt("./data/position_stack.txt", position_stack)
 np.savetxt("./data/profit_tick.txt", profit_tick)
+np.savetxt("./data/book.txt", book)
 
 
 	
