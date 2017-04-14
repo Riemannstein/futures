73656b3e23ad0ba628eb067a9a2cd61f644a0278
Riemannstein <houyifeng1005@hotmail.com>
2017-04-14 18:33:09 +0800

    update

--- ladder_all/ols.py
+++ ladder_all/ols.py
@@ -13,26 +13,38 @@ profit_array = np.loadtxt("profit_array_"+data_date+".txt")
 # Read the open price array
 open_price_array = np.loadtxt("open_price_array_"+data_date+".txt")
 
-# Read the stv_to_disp array
-stv_to_disp = np.loadtxt("stv_to_disp_array_"+data_date_before+".txt")
+# Read the tv_to_disp array
+tv_to_disp_array = np.loadtxt("tv_to_disp_array_"+data_date_before+".txt")
 
 # Compute the normalized profit array
 profit_normalized = np.divide(profit_array, open_price_array)
 
+# COmpute the log of total variation
+log_tv_to_disp_array = np.log(tv_to_disp_array)
+
+
 # Plot normalized profit and stv_to_disp
 
-# Plot the buy, sell information
+# Plot
 for i in range(len(profit_array)):
-	plt.scatter(stv_to_disp[i], profit_normalized[i], color="r")
+	plt.scatter(tv_to_disp_array[i], profit_normalized[i], color="r")
 	plt.xlabel("Total variation (20170413)")
 	plt.ylabel("Profit / openPrice (20170414)")
 plt.savefig("profit_normalized_to_indicator_"+data_date+".eps", format="eps")
 plt.close()
 
-# Plot the buy, sell information
+# Plot
 for i in range(len(profit_array)):
-	plt.scatter(stv_to_disp[i], profit_array[i], color="r")
+	plt.scatter(tv_to_disp_array[i], profit_array[i], color="r")
 	plt.xlabel("Total variation (20170413)")
 	plt.ylabel("Profit (20170414)")
 plt.savefig("profit_to_indicator_"+data_date+".eps", format="eps")
+plt.close()
+
+# Plot
+for i in range(len(profit_array)):
+	plt.scatter(log_tv_to_disp_array[i], profit_normalized[i], color="r")
+	plt.xlabel("log Total variation (20170413)")
+	plt.ylabel("Profit / openPrice (20170414)")
+plt.savefig("profit_normalized_to_log_indicator_"+data_date+".eps", format="eps")
 plt.close()
--- ladder_all/preprocess.py
+++ ladder_all/preprocess.py
@@ -8,12 +8,12 @@ from param import *
 data_date = "20170413"
 print("Data date is ", data_date)
 
-# Initialize stv_to_disp_array
-stv_to_disp_array = np.zeros(len(ticker_list))
+# Initialize tv_to_disp_array
+tv_to_disp_array = np.zeros(len(ticker_list))
 
 for i in range(len(ticker_list)):
 	# Read the data
-	df = pd.read_csv("../data/tick_"+data_date+"/"+ticker_list[i]+"_"+data_date+".csv", encoding = "GBK")
+	df = pd.read_csv("../data/tick_"+data_date_before+"/"+ticker_list[i]+"_"+data_date_before+".csv", encoding = "GBK")
 	
 	start_index = 0 # get the start index of the dataframe
 	
@@ -27,29 +27,29 @@ for i in range(len(ticker_list)):
 	df = df.iloc[start_index:]
 	
 	# Sum of the absolute value of the change in price
-	stv = 0.0
+	tv = 0.0
 
 	# Absolute value of the difference between close and open price 
 	displacement = abs( df.iloc[-1]["closePrice"] - df.iloc[-1]["openPrice"] )
 	
 	# Compute the total variation to displacement ratio
-	stv_to_disp = 0.0
+	tv_to_disp = 0.0
 
 	for j in range(len(df)):
 		if j == 0:
-			stv = 0.0
+			tv = 0.0
 		else:
-			stv += abs( df.iloc[j]["lastPrice"] - df.iloc[j-1]["lastPrice"] )
+			tv += abs( df.iloc[j]["lastPrice"] - df.iloc[j-1]["lastPrice"] )
 
-	print("The total variation of the lastPrice series is ", stv)
+	print("The total variation of the lastPrice series is ", tv)
 
 	if displacement == 0:
-		stv_to_disp = float("inf")
+		tv_to_disp = float("inf")
 	else:
-		stv_to_disp = stv/float(displacement)
+		tv_to_disp = tv/float(displacement)
 	
-	stv_to_disp_array[i] = stv_to_disp
+	tv_to_disp_array[i] = tv_to_disp
 
-np.savetxt("stv_to_disp_array_"+data_date+".txt", stv_to_disp_array)
+np.savetxt("tv_to_disp_array_"+data_date+".txt", tv_to_disp_array)
 
 
